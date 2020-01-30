a3a81e7606bccacecb91c552ca2376b784ffe49d
Golam Sarwar <32290268+sarwar12@users.noreply.github.com>
2020-01-30 09:38:25 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,9 @@
+$(document).ready(function($){
+	// Tooltip js
+	 $('[data-toggle="tooltip"]').tooltip();
+	 
+	//Popover Js
+	 $('[data-toggle="popover"]').popover({
+		   trigger: 'focus',
+	 });
+});
